7f339d7987c670fa1e6f4f1b6d9ffa462bc12bb9
Igor Tikhanovich <tikhanovich.89@gmail.com>
2020-02-13 15:58:55 +0300

    responsive

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,53 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+    $(window).on('load scroll', function(e) {
+        var scroll = document.documentElement.scrollTop;
+        if (scroll == 0) scroll = window.pageYOffset;
+
+        if (scroll > 100) {
+            $('header').addClass('fixed');
+        } else {
+            $('header').removeClass('fixed');
+        }
+    });
+
+    var owl = $('.owl-slider');
+
+    owl.owlCarousel({
+        loop: true,
+        items: 1,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 5000,
+        onInitialized: dotNumbers,
+        onRefreshed: sliderAnimation,
+        onTranslated: sliderAnimation
+    });
+});
+
+function sliderAnimation(e) {
+    $(e.target)
+        .find('.owl-item:not(.active) .animated')
+        .each(function() {
+            var classname = $(this).data('animation');
+            $(this).removeClass(classname);
+        });
+
+    $(e.target)
+        .find('.owl-item.active .animated')
+        .each(function() {
+            var classname = $(this).data('animation');
+            $(this).addClass(classname);
+        });
+}
+
+function dotNumbers(e) {
+    $(e.target)
+        .find('.owl-dot')
+        .each(function() {
+            $(this)
+                .find('span')
+                .text('0' + ($(this).index() + 1));
+        });
+}
